b56623d606cff7c18d82cb08c322ff1de9de9b38
Sillur <mirek-b@seznam.cz>
2017-09-07 20:55:50 +0200

    Clear view of temp and humi.

--- meteostation/meteostation.ino
+++ meteostation/meteostation.ino
@@ -13,6 +13,8 @@ DHT dht(DHTPIN, DHTTYPE);
 
 float temp;
 float humi;
+float newTemp;
+float newHumi;
 
 void setup(){
   Serial.begin(9600);
@@ -29,16 +31,36 @@ void setup(){
 
   lcd.init();                      
   lcd.backlight();
+
+  lcd.setCursor(1,0);
+  lcd.print("Temp:");
+  lcd.setCursor(1,1);
+  lcd.print("Humi:");
   
-  lcd.setCursor(0,0);
-  lcd.print("UNO!");
+  lcd.setCursor(8,0);
+  lcd.print(String(temp) + " C");
   
-  lcd.setCursor(5,0);
-  lcd.print("Temp:" + String(temp));
-  
-  lcd.setCursor(0,1);
-  lcd.print("Humi:" + String(humi));
+  lcd.setCursor(8,1);
+  lcd.print(String(humi) + " %");
 }
 
 void loop(){
+  newTemp = dht.readTemperature();
+  newHumi = dht.readHumidity();
+
+  if (temp != newTemp) {
+    temp = newTemp;
+    
+    lcd.setCursor(8,0);
+    lcd.print(String(temp) + " C");
+  }
+
+  if (humi != newHumi) {
+    humi = newHumi;
+    
+    lcd.setCursor(8,1);
+    lcd.print(String(humi) + " %");
+  }
+
+  delay(1000);
 }
