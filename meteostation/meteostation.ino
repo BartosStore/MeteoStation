5e7ac8fa7cd605d5c2b5e79479ebe766b1faa970
Sillur <mirek-b@seznam.cz>
2017-09-07 20:09:38 +0200

    Temperature and humidity can be read from console.

--- meteostation/meteostation.ino
+++ meteostation/meteostation.ino
@@ -1,11 +1,32 @@
 //Sillur
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
+#include <DHT.h>
 
 //LiquidCrystal_I2C(uint8_t lcd_Addr,uint8_t lcd_cols,uint8_t lcd_rows)
 LiquidCrystal_I2C lcd(0x27,16,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
+#define DHTPIN 2
+#define DHTTYPE DHT22
+
+DHT dht(DHTPIN, DHTTYPE);
+
+float temp;
+float humi;
+
 void setup(){
+  Serial.begin(9600);
+  dht.begin();
+
+  temp = dht.readTemperature();
+  humi = dht.readHumidity();
+
+  Serial.print("Humidity: ");
+  Serial.print(humi);
+  Serial.print(" %, Temp: ");
+  Serial.print(temp);
+  Serial.println(" Celsius");
+
   lcd.init();                      
   lcd.backlight();
   
